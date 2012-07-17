04aac6b903040f59b1496a4b63c0d2c3e05a748f
Ivan Volosyuk <vol@google.com>
2012-07-17 15:45:09 +1000

    Add bluetooth applet

--- tray.sh
+++ tray.sh
@@ -9,6 +9,7 @@ run gnome-settings-daemon
 run gnome-power-manager
 run nm-applet --sm-disable &
 run kmix --keepvisibility
+run bluetooth-applet
 #sleep 5 
 #exec trayer --expand false --edge top --align right --widthtype request --height 22 --margin 267
 
